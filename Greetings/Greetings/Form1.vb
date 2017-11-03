f7e0b724c4d6ff0310c677553a8023a2ed704aba
ip133225 <ip133225@students.davenport.k12.ia.us>
2017-11-03 11:20:28 -0500

    Greetings

---
+++ Greetings/Greetings/Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnShowGreeting.Click
+        Dim strName As String
+
+        strName = txtUserName.Text
+
+        lblGreeting.Text = strName
+    End Sub
+End Class
